ba7efa7ec0966d9dbad766cff0ed7b7d8d5bb2b3
LauraBernert <46823849+LauraBernert@users.noreply.github.com>
2020-06-07 21:16:02 +0200

    Table messages

--- 02_template/src/persistence/HSQLDB.java
+++ 02_template/src/persistence/HSQLDB.java
@@ -228,11 +228,77 @@ public enum HSQLDB {
         update(sqlStringBuilder03.toString());
     }
 
-    public void insertDataTableChannel(String name, int participant_01, int participant_02) {
+    public void insertDataTableChannel(String name, int participant01, int participant02) {
         StringBuilder sqlStringBuilder = new StringBuilder();
-        sqlStringBuilder.append("INSERT INTO channel (").append("name").append(",").append("participant_01").append(",").append("participant_02").append(")");
+        sqlStringBuilder.append("INSERT INTO channel (").append("name").append(",").append("participant01").append(",").append("participant02").append(")");
         sqlStringBuilder.append(" VALUES ");
-        sqlStringBuilder.append("(").append("'").append(name).append("'").append(participant_01).append(participant_02);
+        sqlStringBuilder.append("(").append("'").append(name).append("'").append(",").append(participant01).append(",").append(participant02);
+        sqlStringBuilder.append(")");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder.toString());
+        update(sqlStringBuilder.toString());
+    }
+
+    // Table messages
+
+    public void dropTableMessages() {
+        System.out.println("--- dropTableMessages");
+
+        StringBuilder sqlStringBuilder = new StringBuilder();
+        sqlStringBuilder.append("DROP TABLE messages");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder.toString());
+
+        update(sqlStringBuilder.toString());
+    }
+
+    public void createTableMessages() {
+        System.out.println("--- createTableMessages");
+
+        StringBuilder sqlStringBuilder01 = new StringBuilder();
+        sqlStringBuilder01.append("CREATE TABLE messages ( ");
+        sqlStringBuilder01.append("id TINYINT NOT NULL").append(",");
+        sqlStringBuilder01.append("participant_from_id TINYINT NOT NULL").append(",");
+        sqlStringBuilder01.append("participant_to_id TINYINT NOT NULL").append(",");
+        sqlStringBuilder01.append("plain_message VARCHAR(50) NOT NULL").append(",");
+        sqlStringBuilder01.append("algorithm_id TINYINT NOT NULL").append(",");
+        sqlStringBuilder01.append("encrypted_message VARCHAR(50) NOT NULL").append(",");
+        sqlStringBuilder01.append("keyfile VARCHAR(50) NOT NULL").append(",");
+        sqlStringBuilder01.append("timestamp INTEGER").append(",");
+        sqlStringBuilder01.append("PRIMARY KEY (id)");
+        sqlStringBuilder01.append(" )");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder01.toString());
+        update(sqlStringBuilder01.toString());
+
+        StringBuilder sqlStringBuilder02 = new StringBuilder();
+        sqlStringBuilder02.append("ALTER TABLE messages ADD CONSTRAINT fkMessages01 ");
+        sqlStringBuilder02.append("FOREIGN KEY (participant_from_id) ");
+        sqlStringBuilder02.append("REFERENCES participants (id) ");
+        sqlStringBuilder02.append("ON DELETE CASCADE");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder02.toString());
+        update(sqlStringBuilder02.toString());
+
+        StringBuilder sqlStringBuilder03 = new StringBuilder();
+        sqlStringBuilder03.append("ALTER TABLE messages ADD CONSTRAINT fkMessages02 ");
+        sqlStringBuilder03.append("FOREIGN KEY (participant_to_id) ");
+        sqlStringBuilder03.append("REFERENCES participants (id) ");
+        sqlStringBuilder03.append("ON DELETE CASCADE");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder03.toString());
+        update(sqlStringBuilder03.toString());
+
+        StringBuilder sqlStringBuilder04 = new StringBuilder();
+        sqlStringBuilder04.append("ALTER TABLE messages ADD CONSTRAINT fkMessages03 ");
+        sqlStringBuilder04.append("FOREIGN KEY (algorithm_id) ");
+        sqlStringBuilder04.append("REFERENCES algorithms (id) ");
+        sqlStringBuilder04.append("ON DELETE CASCADE");
+        System.out.println("sqlStringBuilder : " + sqlStringBuilder04.toString());
+        update(sqlStringBuilder04.toString());
+    }
+
+    public void insertDataTableMessages(int participantFromID, int participantToID, String plainMessage, int algorithmID, String encryptedMessage, String keyfile) {
+        int nextID = getNextID("messages") + 1;
+        StringBuilder sqlStringBuilder = new StringBuilder();
+        sqlStringBuilder.append("INSERT INTO messages (").append("id").append(",").append("participant_from_id").append(",").append("participant_to_id").append("plain_message").append(",").append("algorithm_id").append(",").append("encrypted_message").append(",").append("keyfile").append(",").append("timestamp").append(")");
+        sqlStringBuilder.append(" VALUES ");
+        sqlStringBuilder.append("(").append(nextID).append(",").append(participantFromID).append(",").append(participantToID).append(",").append("'").append(plainMessage).append("'").append(",").append(algorithmID).append(",").append("'").append(encryptedMessage).append("'").append(",").append("'").append(keyfile).append("'").append(",").append(System.currentTimeMillis());
         sqlStringBuilder.append(")");
         System.out.println("sqlStringBuilder : " + sqlStringBuilder.toString());
         update(sqlStringBuilder.toString());
